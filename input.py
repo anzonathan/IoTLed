4b818338265c54237c75273d2657f7df428b4310
Nathaniel Mugenyi <anzonathan@gmail.com>
2025-02-27 05:36:36 +0300

    Add: Arduino and python serial coms

---
+++ input.py
@@ -0,0 +1,44 @@
+//IOT LED 
+
+//Led 
+int led = 13;
+
+//Python Value from Host PC
+int input;
+
+void setup() {
+  //Serial Monitor to display Logs
+  Serial.begin(115200);
+  Serial.setTimeout(1); //yet to know what this does
+
+  //Declaring the led as an output
+  pinMode(led,OUTPUT);
+
+
+}
+
+void loop() {
+  //Listening for host serial
+  while (!Serial.available());
+
+  //Convert serial reading to integer: Try Serial.Read
+  input = Serial.readString().toInt();
+  //input = Serial.read();
+
+
+  if (input == 1){
+    digitalWrite(led, HIGH);
+    Serial.println("On");
+  }
+
+  else if (input == 2){
+    digitalWrite(led, LOW);
+    Serial.println("Off");
+  }
+
+  else {Serial.println("Invalid");}
+
+
+  
+
+}
