76f00156891fa264dcc3269b8b1abaf7f6221645
Nathaniel Mugenyi <anzonathan@gmail.com>
2025-02-27 06:39:26 +0300

    Add: API Methods

---
+++ API.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+#allow all connections: Specify Ip address for security
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+switchState = 0; 
+
+@app.get("/")
+def readRoot():
+    return {'State':'API Is connected'}
+
+@app.get("/state/")
+def readRoot():
+    return {'State':switchState}
+
+
+@app.post("/on/")
+def switchOn():
+    switchState = 1
+    return {'State':switchState}
+
+@app.post("/off/")
+def switchOff():
+    switchState = 0
+    return {'State':switchState}
